bd65df6a56f45aeaa27dd7ffe8d1a5e1cea1514b
Catherine Butler <catherinebutler@Catherines-MacBook-Air.local>
2021-08-24 20:52:58 +0100

    Help modal psuedo code

--- src/components/HelpModal/HelpModal.js
+++ src/components/HelpModal/HelpModal.js
@@ -1,3 +1,18 @@
+{
+  /* 
+PSUEDO CODE
+
+1. If help button is clicked display a hidden information page & blur homepage background 
+2. If help button or close button is clicked hide infomation page & remove blur
+
+NEED
+
+- help button
+- close button 
+
+*/
+}
+
 import React from "react";
 
 export default function HelpModal() {

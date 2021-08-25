4824c67616920addb5d7435382cd5e0bb789b73c
natpinnock <natpinnock@gmail.com>
2021-08-25 08:58:14 +0100

    initial menu bar setup

--- src/components/SearchSection/SearchSection.js
+++ src/components/SearchSection/SearchSection.js
@@ -5,8 +5,13 @@ import HeroSection from "../HeroSection/HeroSection";
 
 export default function SearchSection() {
   return (
-    <div>
+    <div id="search-section">
       <h1>Search Section Component</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sit
+        corrupti corporis nihil quae amet ipsa quo quaerat rerum, numquam cum
+        saepe modi in obcaecati debitis necessitatibus minus aspernatur illum.
+      </p>
       <SearchCard />
       <HeroSection />
     </div>

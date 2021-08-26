d53d2b0d1877ba6a7dd792532502153ac3bbe0c4
natpinnock <natpinnock@gmail.com>
2021-08-26 08:34:02 +0100

    journey distance in km logged to the console

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,6 +15,15 @@ export default function App() {
   };
   const [formData, updateFormData] = useState(initialFormData);
 
+  const key = "2Ms4naKG2GfSDZTrwZCG35OhUu0pI";
+  async function getDistance() {
+    let response = await fetch(
+      `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${formData.from}&destinations=${formData.to}&key=${key}`
+    );
+    let data = await response.json();
+    console.log(`Your Journey is: ${data.rows[0].elements[0].distance.text}`);
+  }
+
   const handleChange = (e) => {
     updateFormData({
       ...formData,
@@ -29,7 +38,9 @@ export default function App() {
     setTimeout(() => {
       window.location = "#results-section";
     }, 3000);
+    getDistance();
   };
+
   return (
     <div className="app">
       {/* to be fixed at the top of the page? */}
--- src/components/HelpModal/HelpModal.js
+++ src/components/HelpModal/HelpModal.js
@@ -4,6 +4,12 @@ export default function HelpModal() {
   return (
     <div>
       <h1>Help Modal Component</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur animi
+        exercitationem, autem asperiores harum reiciendis explicabo architecto
+        error tempora? Iure accusantium blanditiis culpa magni vero consectetur
+        praesentium sequi nobis sed.
+      </p>
     </div>
   );
 }

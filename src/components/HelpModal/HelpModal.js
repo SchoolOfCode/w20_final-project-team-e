24857f6ba02fc5a0833b872ef4733b88e640c653
Catherine Butler <catherinebutler@catherines-air.home>
2021-08-29 11:38:34 +0100

    added modal text

--- src/components/HelpModal/HelpModal.js
+++ src/components/HelpModal/HelpModal.js
@@ -19,19 +19,33 @@ export default function HelpModal({ closeModal }) {
           <h1>About our Eco Journey Planner</h1>
         </div>
         <div className="modalBody1">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+          <p>
+            Emission Impossible is on a journey to help make emissions
+            impossible. We’ll help you get better informed about your journey’s
+            environmental impact, routes and modes of transport that can help
+            cut down on emissions or ways to offset your carbon quota. Pop in
+            where you want to go and we’ll help you plan a greener route!{" "}
+          </p>
         </div>
         <div className="modalTitle2">
           <h1>Creating an account</h1>
         </div>
         <div className="modalBody2">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+          <p>
+            Creating an account helps you track your routes and get better
+            informed about the shoe size of carbon footprint over time.{" "}
+          </p>
         </div>
         <div className="modalTitle3">
           <h1>Connect with </h1>
         </div>
         <div className="modalBody3">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+          <p>
+            Tackling the dilemmas around the environment can often feel like a
+            solo slog. Connect with our community of eco journey planners,
+            asking questions, finding solutions and plotting a greener route for
+            the future.{" "}
+          </p>
         </div>
       </div>
     </div>

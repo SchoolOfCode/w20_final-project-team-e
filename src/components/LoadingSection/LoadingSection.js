3a1e3bd64b9985f67b00ad6a2297182c1554cc45
Jamie Hannah <jamiehannah1047@gmail.com>
2021-08-31 18:04:54 +0100

    Modal help button icons added. Illustration for help modal added

--- src/components/LoadingSection/LoadingSection.js
+++ src/components/LoadingSection/LoadingSection.js
@@ -5,25 +5,8 @@ import LoadingImage from "../LoadingImage/LoadingImage";
 export default function LoadingSection() {
   return (
     <div id="loading-section">
-      <h1>Loading Section Component</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, autem
-        nesciunt. Esse reiciendis, aperiam, minus sed neque dignissimos natus
-        illo totam iste labore velit, commodi sit est? Quis, illum. Nihil! Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Dolore iusto tenetur
-        ex recusandae autem beatae illum esse fugiat id iure necessitatibus,
-        sunt reprehenderit, excepturi voluptas et amet perspiciatis. Aliquid,
-        earum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, autem
-        nesciunt. Esse reiciendis, aperiam, minus sed neque dignissimos natus
-        illo totam iste labore velit, commodi sit est? Quis, illum. Nihil! Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Dolore iusto tenetur
-        ex recusandae autem beatae illum esse fugiat id iure necessitatibus,
-        sunt reprehenderit, excepturi voluptas et amet perspiciatis. Aliquid,
-        earum.
-      </p>
+      <h3>Take a big breath</h3>
+      <p>We're just calculating the emissions for your journey.</p>
       <LoadingImage />
     </div>
   );

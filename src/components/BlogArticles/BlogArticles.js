332081daba2fe71adff01a6487e26a8824a027f4
Catherine Butler <catherinebutler@Catherines-MacBook-Air.local>
2021-09-11 13:49:54 +0100

    added image imports to blog article js

--- src/components/BlogArticles/BlogArticles.js
+++ src/components/BlogArticles/BlogArticles.js
@@ -1,4 +1,13 @@
 import React from "react";
+import EnviroImgArt1 from "../../images/Bower-Collective-Img.jpeg";
+import EnviroImgArt2 from "../../images/ecofootprint.png";
+import EnviroImgArt3 from "../../images/offset-expo.png";
+import TravelImgArt1 from "../../images/environment-plane-travel.png";
+import TravelImgArt2 from "../../images/Greener-transport.png";
+import TravelImgArt3 from "../../images/uk-car-emissions.webp";
+import TipsImgArt1 from "../../images/nula.png";
+import TipsImgArt2 from "../../images/worldnomads.png";
+import TipsImgArt3 from "../../images/guardian.png";
 
 import "./BlogArticles.css";
 
@@ -6,7 +15,11 @@ export default function BlogArticles(props) {
   return (
     <section>
       <div class="article-card">
-        <img src="..." class="card-img-top" alt="..."></img>
+        <img
+          src={EnviroImgArt1}
+          // class="card-img-top"
+          alt="grapefruit"
+        ></img>
         <div class="card-body">
           <p class="card-text">
             Some quick example text to build on the card title and make up the

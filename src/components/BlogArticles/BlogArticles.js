d2d3d8bdf187d8a5642120b2f39e071ae3aa3596
Catherine Butler <catherinebutler@Catherines-MacBook-Air.local>
2021-09-11 14:32:33 +0100

    added imgs and info for environmental article block

--- src/components/BlogArticles/BlogArticles.js
+++ src/components/BlogArticles/BlogArticles.js
@@ -1,16 +1,15 @@
 import React from "react";
-import EnviroImgArt1 from "../../images/Bower-Collective-Img.jpeg";
-import EnviroImgArt2 from "../../images/ecofootprint.png";
-import EnviroImgArt3 from "../../images/offset-expo.png";
-import TravelImgArt1 from "../../images/environment-plane-travel.png";
-import TravelImgArt2 from "../../images/Greener-transport.png";
-import TravelImgArt3 from "../../images/uk-car-emissions.webp";
-import TipsImgArt1 from "../../images/nula.png";
-import TipsImgArt2 from "../../images/worldnomads.png";
-import TipsImgArt3 from "../../images/guardian.png";
+import EnviroImgArt1 from "../../images/ArticleImages/ecofootprint.PNG";
+import EnviroImgArt2 from "../../images/ArticleImages/plastic-calc-site.jpeg";
+import EnviroImgArt3 from "../../images/ArticleImages/offsetting-expo.png";
+// import TravelImgArt1 from "../../images/ArticleImages/environment-plane-travel.png";
+// import TravelImgArt2 from "../../images/ArticleImages/greener-transport.png";
+// import TravelImgArt3 from "../../images/ArticleImages/uk-car-emissions.webp";
+// import TipsImgArt1 from "../../images/ArticleImages/nula.png";
+// import TipsImgArt2 from "../../images/ArticleImages/worldnomads.png";
+// import TipsImgArt3 from "../../images/ArticleImages/guardian.png";
 
 import "./BlogArticles.css";
-import ecofootprint from "../../images/ArticleImages/ecofootprint.PNG";
 
 export default function BlogArticles(props) {
   return (
@@ -19,58 +18,58 @@ export default function BlogArticles(props) {
         <img
           src={EnviroImgArt1}
           // class="card-img-top"
-          alt="grapefruit"
+          alt="illustrated landscape with trees"
         ></img>
         <div class="card-body">
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <a
+            href="https://www.footprintcalculator.org/home/en"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </a>
         </div>
-        <a
-          href="https://www.footprintcalculator.org/home/en"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={ecofootprint} class="card-img-top" alt="text"></img>
-          <div class="card-body">
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </a>
       </div>
       <div class="article-card">
-        <a
-          href="https://bowercollective.com/pages/waste-calculator#intro"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src="..." class="card-img-top" alt="..."></img>
-          <div class="card-body">
+        <img
+          src={EnviroImgArt2}
+          class="card-img-top"
+          alt="sliced grapefruit, cacti and plastic bottles"
+        ></img>
+        <div class="card-body">
+          <a
+            href="https://bowercollective.com/pages/waste-calculator#intro"
+            target="_blank"
+            rel="noreferrer"
+          >
             <p class="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
-          </div>
-        </a>
+          </a>
+        </div>
       </div>
-
       <div class="article-card">
-        <a
-          href="https://www.vox.com/2020/2/27/20994118/carbon-offset-climate-change-net-zero-neutral-emissions"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src="..." class="card-img-top" alt="..."></img>
-          <div class="card-body">
+        <img
+          src={EnviroImgArt3}
+          class="card-img-top"
+          alt=" colourful collage of pics of waste, factories with emissions"
+        ></img>
+        <div class="card-body">
+          <a
+            href="https://www.vox.com/2020/2/27/20994118/carbon-offset-climate-change-net-zero-neutral-emissions"
+            target="_blank"
+            rel="noreferrer"
+          >
             <p class="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
-          </div>
-        </a>
+          </a>
+        </div>
       </div>
     </section>
   );

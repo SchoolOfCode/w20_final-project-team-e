82e5d72c2bd3cfa3a4faa12d535bfb4f11af3765
natpinnock <natpinnock@gmail.com>
2021-09-20 20:28:33 +0100

    footer amended - image removed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,20 +5,10 @@ import helpModalImage from "../../images/help_modal_illustration.png";
 export default function Footer(props) {
   return (
     <footer className="footerContainer">
-      <div className="footerText">
-        <p id="footerHeader">WE ARE EMISSION IMPOSSIBLE</p>
-        <p>Find us on GitHub:</p>
-        <a href="https://github.com/catherine-butler">Catherine Butler</a>
-        <a href="https://github.com/JHannah30">Jamie Hannah</a>
-        <a href="https://github.com/natpinnock">Natalie Pinnock</a>
-      </div>
-      <div className="footerImage">
-        <img
-          className="footerImage"
-          src={helpModalImage}
-          alt="lady with suitcase"
-        ></img>
-      </div>
+      <p className="footerHeader">Find us on GitHub:</p>
+      <a href="https://github.com/catherine-butler">Catherine Butler</a>
+      <a href="https://github.com/JHannah30">Jamie Hannah</a>
+      <a href="https://github.com/natpinnock">Natalie Pinnock</a>
     </footer>
   );
 }
